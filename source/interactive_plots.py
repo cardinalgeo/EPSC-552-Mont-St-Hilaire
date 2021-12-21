77440d87bc8e9a6debb5c1fb7dbf8d2311d4e3ec
Robert Collar <robertcollar@roberts-mbp.lan>
2021-12-21 11:51:22 -0800

    amake interactive plot for calibration

--- source/interactive_plots.py
+++ source/interactive_plots.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from ipywidgets import widgets
 import plotly.graph_objects as go
+from datetime import datetime
 
 def interactive_histogram(button_info, data, x_axis_label, y_axis_label): 
 
@@ -293,6 +294,79 @@ def interactive_linear_regression_plot(button_info, model_results, x_axis_label,
     g.layout.yaxis.title = y_axis_label
 
     g.layout.xaxis.tickvals = [datetime.fromordinal(date) for date in model_results[col]["x_predict"].squeeze()]
+
+    # set each button to update according to button filtering
+    button_dict["data"].observe(response, names="value")
+
+    # display histogram   
+    return widgets.VBox([widgets.HBox([button_dict["data"]]), g])
+
+def interactive_linear_regression_calibration_plot(button_info, model_results, x_axis_label, y_axis_label, title): 
+
+    # initialize dictionary to store buttons
+    button_dict = {"data": None }
+
+    # create button to filter data columns and store in dict
+    widget = widgets.Dropdown(
+        options=button_info["data"]["columns"],
+        description=button_info["data"]["name"]
+    )
+    button_dict["data"] = widget
+
+    # create figure
+    g = go.FigureWidget(layout_title_text=title)
+
+    # create histogram traces for each data column
+    for col in button_info["data"]["columns"]: 
+        r_squared = round(model_results[col]["score"], 3)
+        g.add_trace(
+            go.Scatter(x=model_results[col]["x_train"].squeeze(),
+                       y=model_results[col]["y_train"], 
+                       name="data",
+                       mode="markers"
+            )       
+        )        
+        g.add_trace(
+            go.Scatter(x=model_results[col]["y_predict"], 
+                       y=model_results[col]["x_predict"].squeeze(), 
+                       name=f"line of best fit (R squared = {r_squared})",
+                       mode="lines"
+            )       
+        )
+
+
+    def validate(): # function to ensure that value delivered by button is in dataframe    
+        validation_bool = []
+
+        # ensure that data column filter button option is in dataframe
+        if button_dict["data"].value in model_results.keys(): 
+                validation_bool.append(True)
+        else: 
+                validation_bool.append(False)
+
+        # ensure that value condition is met for all buttons
+        if False not in validation_bool:    
+            return True
+        else:
+            return False
+
+    def response(change): # function to dictate data filtering update upon change
+        if validate(): # if all button values are in dataframe
+
+            # set visibility for each column/trace based on data filter button
+            visibilities = [False] * len(g.data) # initialize visibilities for traces (data columns)
+            visible_index = button_info["data"]["columns"].index(button_dict["data"].value) * 2 # get index for data button value
+            visibilities[visible_index] = True # set the visibility of that column/trace to visible
+            visibilities[visible_index+1] = True
+
+            # set data for histogram based on filtering from category buttons and data button
+            with g.batch_update(): # required to update data
+                for i, visibility in enumerate(visibilities): 
+                    g.data[i].visible = visibility
+
+    # label histogram
+    g.layout.xaxis.title = x_axis_label
+    g.layout.yaxis.title = y_axis_label
 
     # set each button to update according to button filtering
     button_dict["data"].observe(response, names="value")
